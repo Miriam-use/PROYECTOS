622556366d5659ee1037145446d237da85793a48
Miriam-use <56627040+Miriam-use@users.noreply.github.com>
2020-02-03 18:23:09 +0100

    Create Puja.java

---
+++ Subastas/src/subasta/Puja.java
@@ -0,0 +1,42 @@
+package subasta;
+
+import subasta.Usuarios;
+
+/**
+ * Una puja representa la cantidad de dinero que ofrece un usuario por 
+ * el producto de una subasta
+ * 
+ * @author JoseRuso
+ * @author Juan
+ * @author Miriam
+ *
+ */
+public class Puja {
+
+	private final Usuarios pujador;
+	private final double cantidad;
+/**
+ * 	
+ * @param pujador
+ * @param cantidad
+ */
+	Puja(Usuarios pujador, double cantidad) {
+		this.pujador = pujador;
+		this.cantidad = cantidad;
+	}
+
+	public Usuarios getPujador() {
+		return pujador;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getName() + " [pujador=" + pujador.getNombre() 
+									+ ", cantidad=" + cantidad 
+									+ "]";
+	}
+}
